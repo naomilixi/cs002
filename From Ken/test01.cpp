ff8fcf3b85ce1d01a23084116007185829bd3f11
lhcdims <ken@zephan.top>
2020-09-27 15:02:53 +0800

    From Ken 01

---
+++ From Ken/test01.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+// int main() {
+//   string strInputA;
+//   int intA, intB;
+
+//   cout << "What is strInputA: ";
+//   getline(cin, strInputA);
+
+//   cout << "You type: " << strInputA << "\n";
+// }
+
+int main() {
+  string strInputA;
+  int intA, intB;
+
+  cout << "What is intA: ";
+  cin >> intA;
+  cin.ignore();
+
+  cout << "What is strInputA: ";
+  getline(cin, strInputA);
+
+  cout << "You type: " << intA << "\n";
+  cout << "You type: " << strInputA << "\n";
+}
---
+++ From Ken/test02.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int funFactorial(int intParm) {
+  int intFactResult = 0;
+
+  // Change intFactResult
+
+
+
+  return intFactResult;
+}
+
+int main() {
+  int intN;
+  int intResult;
+
+  cout << "Input a number: ";
+  cin >> intN;
+
+  intResult = funFactorial(intN);
+
+  cout << "The Factorial of the number is: " << intResult << "\n";
+}
